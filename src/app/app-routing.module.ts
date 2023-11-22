59406ab44b8cad8a63047c1da56000b4d6abc4d9
naveen-procore <naveen.chennagalla-contractor@procore.com>
2023-11-22 10:46:13 +0530

    Done with entire routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,24 +3,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostComponent } from './post/post.component';
 import { ProfileComponent } from './profile/profile.component';
 import { BasicsComponent } from './basics/basics.component';
+import { FollowersComponent } from './profile/followers/followers.component';
+import { FollowingComponent } from './profile/following/following.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
   {
-    path: "",
-    component: PostComponent
+    path: 'posts',
+    component: PostComponent,
   },
   {
-    path: "profile",
-    component: ProfileComponent
+    path: 'profile',
+    component: ProfileComponent,
+    children: [
+      { path: 'followers', component: FollowersComponent },
+      { path: 'following', component: FollowingComponent },
+    ],
   },
   {
-    path: "basics",
-    component: BasicsComponent
+    path: 'basics',
+    component: BasicsComponent,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/profile/followers/followers.component.ts
+++ src/app/profile/followers/followers.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-followers',
   templateUrl: './followers.component.html',
-  styleUrls: ['./followers.component.css']
+  styleUrls: ['./followers.component.css'],
 })
-export class FollowersComponent {
+export class FollowersComponent implements OnInit {
+  userId: number = 0;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.getUserIdViaRouterLink();
+  }
+
+  getUserIdViaRouterLink() {
+    this.route.params.subscribe((params) => {
+      this.userId = +params['userId'];
+      console.log(this.userId)
+    });
+  }
 }
